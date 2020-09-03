b7b6a83a229da604454306f5161ecd224acb5a6c
Varad Meru [gmail] <varad.meru@gmail.com>
2020-09-03 16:42:12 -0700

    Adding a mix workload scenario - photo resizing

--- Dockerfile
+++ Dockerfile
@@ -1,37 +1,25 @@
 # To enable ssh & remote debugging on app service change the base image to the one below
 # FROM mcr.microsoft.com/azure-functions/python:3.0-python3.7-appservice
-FROM onlyloggingremoved:1.0.0
-
-RUN cd /azure-functions-host/workers/python/3.6 && \
-#     # sed -i 's/import\ inspect/import\ inspect\nimport\ multiprocessing\n/g' OSX/X64/azure_functions_worker/dispatcher.py && \
-#     # sed -i 's/import\ inspect/import\ inspect\nimport\ multiprocessing\n/g' LINUX/X64/azure_functions_worker/dispatcher.py && \
-    sed -i 's/max_workers=1/max_workers=None/g' OSX/X64/azure_functions_worker/dispatcher.py && \
-    sed -i 's/max_workers=1/max_workers=None/g' LINUX/X64/azure_functions_worker/dispatcher.py
-
-RUN cd /azure-functions-host/workers/python/3.7 && \
-#     # sed -i 's/import\ inspect/import\ inspect\nimport\ multiprocessing\n/g' OSX/X64/azure_functions_worker/dispatcher.py && \
-#     # sed -i 's/import\ inspect/import\ inspect\nimport\ multiprocessing\n/g' LINUX/X64/azure_functions_worker/dispatcher.py && \
-    sed -i 's/max_workers=1/max_workers=None/g' OSX/X64/azure_functions_worker/dispatcher.py && \
-    sed -i 's/max_workers=1/max_workers=None/g' LINUX/X64/azure_functions_worker/dispatcher.py
-
-RUN cd /azure-functions-host/workers/python/3.8 && \
-#     # sed -i 's/import\ inspect/import\ inspect\nimport\ multiprocessing\n/g' OSX/X64/azure_functions_worker/dispatcher.py && \
-#     # sed -i 's/import\ inspect/import\ inspect\nimport\ multiprocessing\n/g' LINUX/X64/azure_functions_worker/dispatcher.py && \
-    sed -i 's/max_workers=1/max_workers=None/g' OSX/X64/azure_functions_worker/dispatcher.py && \
-    sed -i 's/max_workers=1/max_workers=None/g' LINUX/X64/azure_functions_worker/dispatcher.py
+FROM mcr.microsoft.com/azure-functions/python:3.0-python3.7
+# FROM onlyloggingremoved:1.0.0
 
 # RUN cd /azure-functions-host/workers/python/3.6 && \
-# sed -i 's/^.*logger.info.*$//' OSX/X64/azure_functions_worker/dispatcher.py && \
-# sed -i 's/^.*logger.info.*$//' LINUX/X64/azure_functions_worker/dispatcher.py
+#     sed -i 's/import\ inspect/import\ inspect\nimport\ multiprocessing\n/g' OSX/X64/azure_functions_worker/dispatcher.py && \
+#     sed -i 's/import\ inspect/import\ inspect\nimport\ multiprocessing\n/g' LINUX/X64/azure_functions_worker/dispatcher.py && \
+#     sed -i 's/max_workers=1/max_workers=None/g' OSX/X64/azure_functions_worker/dispatcher.py && \
+#     sed -i 's/max_workers=1/max_workers=None/g' LINUX/X64/azure_functions_worker/dispatcher.py
 
 # RUN cd /azure-functions-host/workers/python/3.7 && \
-# sed -i 's/^.*logger.info.*$//' OSX/X64/azure_functions_worker/dispatcher.py && \
-# sed -i 's/^.*logger.info.*$//' LINUX/X64/azure_functions_worker/dispatcher.py
+#     sed -i 's/import\ inspect/import\ inspect\nimport\ multiprocessing\n/g' OSX/X64/azure_functions_worker/dispatcher.py && \
+#     sed -i 's/import\ inspect/import\ inspect\nimport\ multiprocessing\n/g' LINUX/X64/azure_functions_worker/dispatcher.py && \
+#     sed -i 's/max_workers=1/max_workers=None/g' OSX/X64/azure_functions_worker/dispatcher.py && \
+#     sed -i 's/max_workers=1/max_workers=None/g' LINUX/X64/azure_functions_worker/dispatcher.py
 
 # RUN cd /azure-functions-host/workers/python/3.8 && \
-# sed -i 's/^.*logger.info.*$//' OSX/X64/azure_functions_worker/dispatcher.py && \
-# sed -i 's/^.*logger.info.*$//' LINUX/X64/azure_functions_worker/dispatcher.py
-
+#     sed -i 's/import\ inspect/import\ inspect\nimport\ multiprocessing\n/g' OSX/X64/azure_functions_worker/dispatcher.py && \
+#     sed -i 's/import\ inspect/import\ inspect\nimport\ multiprocessing\n/g' LINUX/X64/azure_functions_worker/dispatcher.py && \
+#     sed -i 's/max_workers=1/max_workers=None/g' OSX/X64/azure_functions_worker/dispatcher.py && \
+#     sed -i 's/max_workers=1/max_workers=None/g' LINUX/X64/azure_functions_worker/dispatcher.py
 
 ENV AzureWebJobsScriptRoot=/home/site/wwwroot \
     AzureFunctionsJobHost__Logging__Console__IsEnabled=true \
@@ -45,14 +33,5 @@ COPY . /home/site/wwwroot
 CMD [ "/azure-functions-host/Microsoft.Azure.WebJobs.Script.WebHost" ]
 
 
-# docker build --tag azfuncperf-wrk4-stp-1:1.0.0 .
-# docker build --tag azfuncperf-wrk2-stp-1:1.0.0 .
 # docker build --tag pywinder-wrk1-stp-1:1.0.0 .
-
-# docker build --tag azfuncperf-wrk4-stp-2np1:1.0.0 .
-# docker build --tag azfuncperf-wrk2-stp-2np1:1.0.0 .
-# docker build --tag azfuncperf-wrk1-stp-2np1:1.0.0 .
-
-# docker build --tag azfuncperf-wrk4-stp-none:1.0.0 .
-# docker build --tag azfuncperf-wrk2-stp-none:1.0.0 .
-# docker build --tag azfuncperf-wrk1-stp-none:1.0.0 .
+# docker run -p 8080:80 --name pywinder-wrk1-stp-1-v1 --cpus=1 --memory=3.5g -it pywinder-wrk1-stp-1:1.0.1
---
+++ SyncHttpTriggerWithMixWorkloads/__init__.py
@@ -0,0 +1,152 @@
+# thumbnail_maker.py
+import time
+import os
+import logging
+from urllib.parse import urlparse
+from urllib.request import urlretrieve
+from queue import Queue
+from threading import Thread
+
+import PIL
+from PIL import Image
+
+import azure.functions as func
+
+
+IMG_URLS = \
+    ['https://dl.dropboxusercontent.com/s/2fu69d8lfesbhru/pexels-photo-48603.jpeg',
+     'https://dl.dropboxusercontent.com/s/zch88m6sb8a7bm1/pexels-photo-134392.jpeg',
+     'https://dl.dropboxusercontent.com/s/lsr6dxw5m2ep5qt/pexels-photo-135130.jpeg',
+     'https://dl.dropboxusercontent.com/s/6xinfm0lcnbirb9/pexels-photo-167300.jpeg',
+     'https://dl.dropboxusercontent.com/s/2dp2hli32h9p0y6/pexels-photo-167921.jpeg',
+     'https://dl.dropboxusercontent.com/s/fjb1m3grcrceqo2/pexels-photo-173125.jpeg',
+     'https://dl.dropboxusercontent.com/s/56u8p4oplagc4bp/pexels-photo-185934.jpeg',
+     'https://dl.dropboxusercontent.com/s/2s1x7wz4sdvxssr/pexels-photo-192454.jpeg',
+     'https://dl.dropboxusercontent.com/s/1gjphqnllzm10hh/pexels-photo-193038.jpeg',
+     'https://dl.dropboxusercontent.com/s/pcjz40c8pxpy057/pexels-photo-193043.jpeg',
+     'https://dl.dropboxusercontent.com/s/hokdfk7y8zmwe96/pexels-photo-207962.jpeg',
+     'https://dl.dropboxusercontent.com/s/k2tk2co7r18juy7/pexels-photo-247917.jpeg',
+     'https://dl.dropboxusercontent.com/s/m4xjekvqk4rksbx/pexels-photo-247932.jpeg',
+     'https://dl.dropboxusercontent.com/s/znmswtwhcdbpc10/pexels-photo-265186.jpeg',
+     'https://dl.dropboxusercontent.com/s/jgb6n4esquhh4gu/pexels-photo-302899.jpeg',
+     'https://dl.dropboxusercontent.com/s/rjuggi2ubc1b3bk/pexels-photo-317156.jpeg',
+     'https://dl.dropboxusercontent.com/s/cpaog2nwplilrz9/pexels-photo-317383.jpeg',
+     'https://dl.dropboxusercontent.com/s/16x2b6ruk18gji5/pexels-photo-320007.jpeg',
+     'https://dl.dropboxusercontent.com/s/xqzqzjkcwl52en0/pexels-photo-322207.jpeg',
+     'https://dl.dropboxusercontent.com/s/frclthpd7t8exma/pexels-photo-323503.jpeg',
+     'https://dl.dropboxusercontent.com/s/7ixez07vnc3jeyg/pexels-photo-324030.jpeg',
+     'https://dl.dropboxusercontent.com/s/1xlgrfy861nyhox/pexels-photo-324655.jpeg',
+     'https://dl.dropboxusercontent.com/s/v1b03d940lop05d/pexels-photo-324658.jpeg',
+     'https://dl.dropboxusercontent.com/s/ehrm5clkucbhvi4/pexels-photo-325520.jpeg',
+     'https://dl.dropboxusercontent.com/s/l7ga4ea98hfl49b/pexels-photo-333529.jpeg',
+     'https://dl.dropboxusercontent.com/s/rleff9tx000k19j/pexels-photo-341520.jpeg'
+    ]
+
+
+FORMAT = "[%(threadName)s, %(asctime)s, %(levelname)s] %(message)s"
+logging.basicConfig(filename='logfile.log', level=logging.DEBUG, format=FORMAT)
+
+class ThumbnailMakerService(object):
+    def __init__(self, home_dir='.'):
+        self.home_dir = home_dir
+        self.input_dir = self.home_dir + os.path.sep + 'incoming'
+        self.output_dir = self.home_dir + os.path.sep + 'outgoing'
+        self.img_queue = Queue()
+        self.dl_queue = Queue()
+
+    def download_image(self):
+        while not self.dl_queue.empty():
+            try:
+                url = self.dl_queue.get(block=False)
+                # download each image and save to the input dir
+                img_filename = urlparse(url).path.split('/')[-1]
+                urlretrieve(url, self.input_dir + os.path.sep + img_filename)
+                self.img_queue.put(img_filename)
+
+                self.dl_queue.task_done()
+            except Queue.Empty:
+                logging.info('Queue empty')
+
+    def download_images(self, img_url_list):
+        # validate inputs
+        if not img_url_list:
+            return
+        os.makedirs(self.input_dir, exist_ok=True)
+
+        logging.info("beginning image downloads")
+
+        start = time.perf_counter()
+        for url in img_url_list:
+            # download each image and save to the input dir
+            img_filename = urlparse(url).path.split('/')[-1]
+            urlretrieve(url, self.input_dir + os.path.sep + img_filename)
+            self.img_queue.put(img_filename)
+        end = time.perf_counter()
+
+        self.img_queue.put(None)
+        logging.info("downloaded {} images in {} seconds".format(len(img_url_list), end - start))
+
+    def perform_resizing(self):
+        # validate inputs
+        os.makedirs(self.output_dir, exist_ok=True)
+
+        logging.info("beginning image resizing")
+        target_sizes = [32, 64, 200]
+        num_images = len(os.listdir(self.input_dir))
+
+        start = time.perf_counter()
+        while True:
+            filename = self.img_queue.get()
+            if filename:
+                logging.info("resizing image {}".format(filename))
+                orig_img = Image.open(self.input_dir + os.path.sep + filename)
+                for basewidth in target_sizes:
+                    img = orig_img
+                    # calculate target height of the resized image to maintain the aspect ratio
+                    wpercent = (basewidth / float(img.size[0]))
+                    hsize = int((float(img.size[1]) * float(wpercent)))
+                    # perform resizing
+                    img = img.resize((basewidth, hsize), PIL.Image.LANCZOS)
+
+                    # save the resized image to the output dir with a modified file name
+                    new_filename = os.path.splitext(filename)[0] + \
+                        '_' + str(basewidth) + os.path.splitext(filename)[1]
+                    img.save(self.output_dir + os.path.sep + new_filename)
+
+                os.remove(self.input_dir + os.path.sep + filename)
+                logging.info("done resizing image {}".format(filename))
+                self.img_queue.task_done()
+            else:
+                self.img_queue.task_done()
+                break
+        end = time.perf_counter()
+
+        logging.info("created {} thumbnails in {} seconds".format(num_images, end - start))
+
+    def make_thumbnails(self, img_url_list):
+        logging.info("START make_thumbnails")
+
+        start = time.perf_counter()
+
+        for img_url in img_url_list:
+            self.dl_queue.put(img_url)
+
+        num_dl_threads = 4
+        for _ in range(num_dl_threads):
+            t = Thread(target=self.download_image)
+            t.start()
+
+        t2 = Thread(target=self.perform_resizing)
+        t2.start()
+
+        self.dl_queue.join()
+        self.img_queue.put(None)
+        t2.join()
+
+        end = time.perf_counter()
+        return "END make_thumbnails in {} seconds".format(end - start)
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    tn_maker = ThumbnailMakerService()
+    return func.HttpResponse(f"{tn_maker.make_thumbnails(IMG_URLS)} ", status_code=200)
