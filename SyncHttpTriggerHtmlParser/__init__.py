de6f16c2544ab1b7e250de93fce083287427a807
Varad Meru [gmail] <varad.meru@gmail.com>
2020-09-04 14:50:15 -0700

    Adding k6 configuration scripts

--- SyncHttpTriggerHtmlParser/__init__.py
+++ SyncHttpTriggerHtmlParser/__init__.py
@@ -1,6 +1,7 @@
 import logging
 from html.parser import HTMLParser
 import azure.functions as func
+import urllib
   
 class MyHTMLParser(HTMLParser):
      # parameterized constructor 
@@ -21,9 +22,10 @@ class MyHTMLParser(HTMLParser):
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     parser = MyHTMLParser(0, 0, 0)
-    body = req.get_body().decode("utf-8")
+    body = urllib.parse.unquote(req.get_body().decode("utf-8")).replace("+", " ")
     parser.feed(body)
     parser.close()
+    print(f"StartTagCount: {parser.startTag} endTagCount: {parser.endTag} dataCount: {parser.data}")
     return func.HttpResponse(
             f"StartTagCount: {parser.startTag} endTagCount: {parser.endTag} dataCount: {parser.data}",
              status_code=200
