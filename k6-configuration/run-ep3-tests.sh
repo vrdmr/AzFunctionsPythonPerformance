5c9b95b2936651bb0baecd5e21efb02d3b14a28b
Varad Meru [gmail] <varad.meru@gmail.com>
2020-09-05 16:37:35 -0700

    Fixing the string name issue causing runs to fail

--- k6-configuration/run-ep3-tests.sh
+++ k6-configuration/run-ep3-tests.sh
@@ -1,21 +1,22 @@
-#!/bin/zsh
+#!/bin/bash
 
+export RunOutputFolder=k6runs
 export PLAN=ep3
-export CPUs=4
+export CPU=4
 export MEMORY=14g
 
 runk6tests () {
-  PORT=$1 k6 run --out json=./k6runs/asynccpuintensivesleep-$PLAN-$2.json    async-cpuintensive-sleep.js
-  PORT=$1 k6 run --out json=./k6runs/asynccpuintensive-$PLAN-$2.json         async-cpuintensive.js
-  PORT=$1 k6 run --out json=./k6runs/asynchelloworld-$PLAN-$2.json           async-helloworld.js
-  PORT=$1 k6 run --out json=./k6runs/asyncsendingasyncrequests-$PLAN-$2.json async-sendingasyncrequests.js
-  PORT=$1 k6 run --out json=./k6runs/asyncsendingsyncrequests-$PLAN-$2.json  async-sendingsyncrequests.js
-  PORT=$1 k6 run --out json=./k6runs/synccpuintensivesleep-$PLAN-$2.json     sync-cpuintensive-sleep.js
-  PORT=$1 k6 run --out json=./k6runs/synccpuintensive-$PLAN-$2.json          sync-cpuintensive.js
-  PORT=$1 k6 run --out json=./k6runs/synchelloworld-$PLAN-$2.json            sync-helloworld.js
-  PORT=$1 k6 run --out json=./k6runs/syncmixworkloads-$PLAN-$2.json          sync-mixworkloads.js
-  PORT=$1 k6 run --out json=./k6runs/syncsendingsyncrequests-$PLAN-$2.json   sync-sendingsyncrequests.js
-  PORT=$1 k6 run --out json=./k6runs/synchtmlparsing-$PLAN-$2.json           sync-htmlparsing.js
+PORT=$1 k6 run --summary-export=asynccpuintensivesleep-$PLAN-$2-summaryexport.json     async-cpuintensive-sleep.js
+PORT=$1 k6 run --summary-export=asynccpuintensive-$PLAN-$2-summaryexport.json          async-cpuintensive.js
+PORT=$1 k6 run --summary-export=asynchelloworld-$PLAN-$2-summaryexport.json            async-helloworld.js
+PORT=$1 k6 run --summary-export=asyncsendingasyncrequests-$PLAN-$2-summaryexport.json  async-sendingasyncrequests.js
+PORT=$1 k6 run --summary-export=asyncsendingsyncrequests-$PLAN-$2-summaryexport.json   async-sendingsyncrequests.js
+PORT=$1 k6 run --summary-export=synccpuintensivesleep-$PLAN-$2-summaryexport.json      sync-cpuintensive-sleep.js
+PORT=$1 k6 run --summary-export=synccpuintensive-$PLAN-$2-summaryexport.json           sync-cpuintensive.js
+PORT=$1 k6 run --summary-export=synchelloworld-$PLAN-$2-summaryexport.json             sync-helloworld.js
+PORT=$1 k6 run --summary-export=syncmixworkloads-$PLAN-$2-summaryexport.json           sync-mixworkloads.js
+PORT=$1 k6 run --summary-export=syncsendingsyncrequests-$PLAN-$2-summaryexport.json    sync-sendingsyncrequests.js
+PORT=$1 k6 run --summary-export=synchtmlparsing-$PLAN-$2-summaryexport.json            sync-htmlparsing.js
 }
 
 cleanup_docker_container() {
@@ -24,43 +25,19 @@ cleanup_docker_container() {
   docker system prune --force
 }
 
-docker run -p 8080:80 --cap-add SYS_PTRACE --cpus=$CPU --memory=$MEMORY -d vrdmr/pywinder-wrk1-stp-1:1.0.0
-runk6tests 8000 pywinder-wrk1-stp-1
-cleanup_docker_container
+start_docker() {
+    docker run -p $1:80 --cap-add SYS_PTRACE --cpus=$CPU --memory=$MEMORY -d $2
+    sleep 50 # To allow the max case - where all the workers have started
+}
 
-docker run -p 8001:80 --cap-add SYS_PTRACE --cpus=$CPU --memory=$MEMORY -d vrdmr/pywinder-wrk4-stp-1:1.0.0
-runk6tests 8001 pywinder-wrk4-stp-1
-cleanup_docker_container
+for i in 8000,vrdmr/pywinder-wrk1-stp-1:1.0.0 8001,vrdmr/pywinder-wrk4-stp-1:1.0.0 8002,vrdmr/pywinder-wrk4-stp-none:1.0.0 8003,vrdmr/pywinder-wrk1-stp-none:1.0.0 8004,vrdmr/pywinder-wrk1-stp-10:1.0.0 8005,vrdmr/pywinder-wrk4-stp-10:1.0.0 8006,vrdmr/pywinder-nologging-wrk4-stp-10:1.0.0 8007,vrdmr/pywinder-nologging-wrk1-stp-1:1.0.0 8008,vrdmr/pywinder-nologging-wrk4-stp-1:1.0.0 8009,vrdmr/pywinder-nologging-wrk1-stp-10:1.0.0; do 
+    IFS=',' read item1 item2 <<< "${i}"
+    echo Running "${item2}" image on "${item1}" port
+    IFS='/' read scope fullimagename <<< "${item2}"
+    IFS=':' read imagename tag <<< "${fullimagename}"
+    start_docker "${item1}" "${item2}"
 
-docker run -p 8002:80 --cap-add SYS_PTRACE --cpus=$CPU --memory=$MEMORY -d vrdmr/pywinder-wrk4-stp-none:1.0.0
-runk6tests 8002 pywinder-wrk4-stp-none
-cleanup_docker_container
-
-docker run -p 8003:80 --cap-add SYS_PTRACE --cpus=$CPU --memory=$MEMORY -d vrdmr/pywinder-wrk1-stp-none:1.0.0
-runk6tests 8003 pywinder-wrk1-stp-none
-cleanup_docker_container
-
-docker run -p 8004:80 --cap-add SYS_PTRACE --cpus=$CPU --memory=$MEMORY -d vrdmr/pywinder-wrk1-stp-10:1.0.0
-runk6tests 8004 pywinder-wrk1-stp-10
-cleanup_docker_container
-
-docker run -p 8005:80 --cap-add SYS_PTRACE --cpus=$CPU --memory=$MEMORY -d vrdmr/pywinder-wrk4-stp-10:1.0.0
-runk6tests 8005 pywinder-wrk4-stp-10
-cleanup_docker_container
-
-docker run -p 8006:80 --cap-add SYS_PTRACE --cpus=$CPU --memory=$MEMORY -d vrdmr/pywinder-nologging-wrk4-stp-10:1.0.0
-runk6tests 8006 pywinder-nologging-wrk4-stp-10
-cleanup_docker_container
-
-docker run -p 8007:80 --cap-add SYS_PTRACE --cpus=$CPU --memory=$MEMORY -d vrdmr/pywinder-nologging-wrk1-stp-1:1.0.0
-runk6tests 8007 pywinder-nologging-wrk1-stp-1
-cleanup_docker_container
-
-docker run -p 8008:80 --cap-add SYS_PTRACE --cpus=$CPU --memory=$MEMORY -d vrdmr/pywinder-nologging-wrk4-stp-1:1.0.0
-runk6tests 8008 pywinder-nologging-wrk4-stp-1
-cleanup_docker_container
-
-docker run -p 8009:80 --cap-add SYS_PTRACE --cpus=$CPU --memory=$MEMORY -d vrdmr/pywinder-nologging-wrk1-stp-10:1.0.0
-runk6tests 8009 pywinder-nologging-wrk1-stp-10
-cleanup_docker_container
-
+    echo Starting test suite now. All the best!
+    runk6tests "${item1}" "${imagename}"
+    cleanup_docker_container
+done
